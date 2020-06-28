18db1b20859b01c8ccc112f87e26ad3b2c7a0526
Martin <martins.rey@gmail.com>
2020-06-28 12:59:32 -0300

    UI: Compile and copy to Api\Public

---
+++ clientApp/build/precache-manifest.a8afd930b75587050d1a0a735411db02.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "391677383ea6e2dc4e830a466426ebf3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "03e3b375021e1c1a51e9",
+    "url": "/static/css/2.8dbe6964.chunk.css"
+  },
+  {
+    "revision": "e3eb6f3c2ae84c54a967",
+    "url": "/static/css/main.f08fe38d.chunk.css"
+  },
+  {
+    "revision": "03e3b375021e1c1a51e9",
+    "url": "/static/js/2.353cc51e.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.353cc51e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3eb6f3c2ae84c54a967",
+    "url": "/static/js/main.c8a888e6.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "2491afe95f7cac40010a141ae5c5fc77",
+    "url": "/static/media/open-sans-v16-cyrillic_latin-regular.2491afe9.woff"
+  },
+  {
+    "revision": "aeb4b1bfc96bce2be7d95d9917d8cccb",
+    "url": "/static/media/open-sans-v16-cyrillic_latin-regular.aeb4b1bf.woff2"
+  }
+]);
